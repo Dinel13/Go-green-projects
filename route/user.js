8080dabcc4b4b05479fded296edcf10aff4b28c5
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-05-18 11:12:48 +0800

    create user route

--- route/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const userController = require("../controllers/user");
-
-const router = express.Router();
-
-router.post("/signup", userController.signup);
-
-module.exports = router;
