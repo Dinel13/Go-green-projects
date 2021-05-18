06d135a6121ded38b35e07ef0f4b5d1234deea89
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-05-18 11:25:22 +0800

    create user login controller

--- controllers/user.js
+++ controllers/user.js
@@ -56,4 +56,57 @@ const signup = async (req, res, next) => {
     .json({ userId: createuser.id, name: createuser.name, token: token });
 };
 
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    let existingUser;
+    try {
+      existingUser = await User.findOne({ email: email });
+    } catch (err) {
+      const error = new HttpError("Gagal masuk, coba lagi nanti.", 500);
+      return next(error);
+    }
+  
+    if (!existingUser) {
+      const error = new HttpError("Credentials tidak cocok.", 401);
+      return next(error);
+    }
+  
+    let isValidPassword = false;
+    try {
+      isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (err) {
+      console.log(err);
+      const error = new HttpError("Tidak bisa masuk, coba lagi nanti.", 500);
+      return next(error);
+    }
+  
+    if (!isValidPassword) {
+      const error = new HttpError(
+        "Tidak bisa masuk, Pastikan password kamu betul.",
+        401
+      );
+      return next(error);
+    }
+  
+    let token;
+    try {
+      token = jwt.sign(
+        { userId: existingUser.id, email: existingUser.email },
+        process.env.JWT_KEY,
+        { expiresIn: "1h" }
+      );
+    } catch (err) {
+      const error = new HttpError("Tidak bisa masuk, coba lagi nanti.", 500);
+      return next(error);
+    }
+  
+    res.json({
+      userId: existingUser.id,
+      name: existingUser.name,
+      token: token,
+    });
+  };
+
 exports.signup = signup;
+exports.login = login;
