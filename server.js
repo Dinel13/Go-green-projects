53cfe33de812566cd2608aebe9846a9388b4073f
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-05-18 11:15:18 +0800

    add user route to server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const userRoute = require("./routes/user");
+
 const app = express();
 
 app.use(express.json({ limit: "20mb" }));
@@ -12,6 +14,8 @@ app.use(cors("*"));
 app.use("/", (req, res, next) => {
   res.status(200).json({ message: "api is working" });
 });
+
+app.use("/api/user", userRoute);
 
 const url = "mongodb://127.0.0.1:27017/green-project";
 mongoose
