97e41bfce3924fe960185bf035727d03a3987177
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-05-21 21:07:22 +0800

    try mysql

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
-function fetchUsername() {
-  return new Promise((resolve, reject) => {
-      setTimeout(() => {
-          resolve('JSUser');
-      }, 3000);
-  })
-}
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-console.log("Fetching username...");
-const username = fetchUsername();
-console.log(`You are logged in as ${username}`);
-console.log("Welcome!");
+const userRoute = require("./routes/user");
+const db = require("./util/database");
+
+const app = express();
+
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: false, limit: "20mb" }));
+
+app.use(cors("*"));
+
+db.execute("SELECT * FROM users")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/test", (req, res, next) => {
+  res.status(200).json({ message: "api is working" });
+});
+
+app.use("/api/user", userRoute);
+
+const url = "mongodb://127.0.0.1:27017/green-project";
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    app.listen(process.env.PORT || 8080);
+  })
+  .catch((err) => console.log(err));
--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 const pool = mysql.createPool({
     host : 'localhost',
     user : 'root',
-    database : 'g-project',
+    database : 'gproject',
     password : 'Dinelos5!'
 })
 
