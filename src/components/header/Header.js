31a1a2f5100903b2f2918dea951f4a17a580dcbd
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-05-29 10:52:04 +0800

    add signup logout action

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import logo from '../../assets/logo.jpg'
-
+import logo from "../../assets/logo.jpg";
 
 export default function Header() {
+  const token = useSelector((state) => state.auth.token);
   return (
     <nav className="flex items-center justify-between flex-wrap bg-teal p-6 bg-green-500">
       <div className="flex items-center flex-no-shrink text-white mr-6">
-        <img className="rounded-full w-10 mr-2"  src={logo} alt="logo" />
+        <img className="rounded-full w-10 mr-2" src={logo} alt="logo" />
         <Link to="/" className="font-semibold text-xl tracking-tight">
           Go Green
         </Link>
@@ -27,7 +28,8 @@ export default function Header() {
       </div>
       <div className="w-full flex-grow lg:items-center lg:w-auto hidden lg:flex">
         <div className="text-sm lg:flex-grow">
-          <Link to="/docs"
+          <Link
+            to="/docs"
             href="#responsive-header"
             className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4"
           >
@@ -39,25 +41,39 @@ export default function Header() {
           >
             Source Code
           </a>
-          <Link to="/suportus"
+          <Link
+            to="/suportus"
             href="#responsive-header"
             className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white"
           >
             Support Us
           </Link>
         </div>
-        <div>
-          <Link to="/login"
-            className="inline-block text-sm mr-2 px-4 py-2 leading-none border rounded text-white border-white hover:text-gray-800 hover:border-transparent hover:bg-white mt-4 lg:mt-0"
-          >
-            Login
-          </Link>
-          <Link to="/signup"
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white bg-green-700 hover:border-transparent hover:text-teal hover:bg-green-800 mt-4 lg:mt-0"
-          >
-            Sign up
-          </Link>
-        </div>
+        {token ? (
+          <div>
+            <button
+              onClick={() => {}}
+              className="inline-block text-sm mr-2 px-4 py-2 leading-none border rounded text-white border-white hover:text-gray-800 hover:border-transparent hover:bg-white mt-4 lg:mt-0"
+            >
+              Log Out
+            </button>
+          </div>
+        ) : (
+          <div>
+            <Link
+              to="/login"
+              className="inline-block text-sm mr-2 px-4 py-2 leading-none border rounded text-white border-white hover:text-gray-800 hover:border-transparent hover:bg-white mt-4 lg:mt-0"
+            >
+              Login
+            </Link>
+            <Link
+              to="/signup"
+              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white bg-green-700 hover:border-transparent hover:text-teal hover:bg-green-800 mt-4 lg:mt-0"
+            >
+              Sign up
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
--- src/store/authAction.js
+++ src/store/authAction.js
@@ -1,6 +1,45 @@
 import { authActions } from "./authSlice";
 import { uiActions } from "./uiSlice";
 
+export const signup = (email, name, password) => {
+  return async (dispatch) => {
+    const signupToBackend = async () => {
+      const res = await fetch(
+        "https://our-philosophy-314515.et.r.appspot.com/api/user/signup",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email,
+            name,
+            password,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = res.json();
+      if (!res.ok) {
+        throw new Error(data.message || "Tidak bisa mendaftar");
+      }
+      return data;
+    };
+    try {
+      const res = await signupToBackend()
+      dispatch(authActions.login(res))
+    } catch (error) {
+      console.log(error);
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Gagal masuk",
+          message: error.message,
+        })
+      );
+    }
+  };
+};
+
 export const login = (email, password) => {
   return async (dispatch) => {
     const loginToBackend = async () => {
@@ -23,7 +62,7 @@ export const login = (email, password) => {
       if (!res.ok) {
         throw new Error(data.message || "Tidak bisa masuk");
       }
-      return res;
+      return data;
     };
 
     try {
@@ -41,3 +80,9 @@ export const login = (email, password) => {
     }
   };
 };
+
+export const logout = () => {
+  return async (dispatch) => {
+    dispatch(authActions.logout)
+  }
+}
