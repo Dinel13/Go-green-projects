80961f7f7a4fa206bd442aa447e1920bff88256c
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-06-03 15:48:00 +0800

    first commit

--- Auth-API/util/sequelize.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequaelize = new Sequelize("gproject", "root", "Dinelos5!", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports = sequaelize;
--- web-front/deploy.md
+++
@@ -1,3 +0,0 @@
-gcloud builds submit --tag gcr.io/our-philosophy-314515/frontend
-
-gcloud beta run deploy --image gcr.io/our-philosophy-314515/frontend --platform managed --region asia-southeast2
