ad60ea293faab7efc4d0e17463cbb7c78b27628c
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-06-07 02:11:29 +0800

    add product model

---
+++ models/Product.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    item: {
+      type: String,
+      require: true,
+      maxLength: 64,
+    },
+    price: {
+      type: Number,
+      require: true,
+      min: 1000,
+      max: 99999999,
+    },
+    sellerId: {
+      type: Object,
+      require: true,
+    },
+    qty: {
+      type: Number,
+      require: true,
+      min: 1000,
+    },
+    category: {
+      type: String,
+      require: true,
+      maxLength: 32,
+    },
+    image: {
+      type: String,
+      require: true,
+      maxLength: 32,
+    },
+    sortDesc: {
+      type: String,
+      require: true,
+      maxLength: 225,
+    },
+    desc: {
+      type: String,
+      require: true,
+      maxLength: 1024,
+    },
+    weight: {
+      type: String,
+      require: true,
+      maxLength: 128,
+    },
+    location: {
+      type: String,
+      require: true,
+      maxLength: 128,
+    },
+  },
+  { timestamps: true }
+);
